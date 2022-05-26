40395ac0a290f555ae6f924bc5ddac501421709f
witherking25 <witherking@withertech.com>
2022-05-25 20:31:35 -0500

    Add config file and filesystem watching

--- src/savesync/app.py
+++ src/savesync/app.py
@@ -7,50 +7,32 @@ import pathlib
 import shlex
 import subprocess
 import pexpect
+import yaml
+from schema import Schema, Optional, SchemaError
+
+schema = Schema(
+    {
+        "remote": str,
+        "sync-cooldown": str,
+        "rclone-args": [str],
+        "unison-args": [str]
+    }
+)
+
+default_schema = Schema(
+    {
+        Optional("remote", default="saves"): str,
+        Optional("sync-cooldown", default="watch"): str,
+        Optional("rclone-args", default=[]): [str],
+        Optional("unison-args", default=[]): [str]
+    }
+)
+
+conf: dict
 
 
 def resource_dir():
     return pathlib.Path(__file__).parent.joinpath("resources")
-
-
-rclone: str = resource_dir().joinpath("bin/rclone").__str__() \
-    if os.environ.get("APPDIR") is None else pathlib.Path(os.environ.get("APPDIR")).joinpath("usr/bin/rclone").__str__()
-unison: str = resource_dir().joinpath("bin/unison").__str__() \
-    if os.environ.get("APPDIR") is None else pathlib.Path(os.environ.get("APPDIR")).joinpath("usr/bin/unison").__str__()
-
-
-def main():
-    parser = argparse.ArgumentParser(prog="savesync", description="Syncs EmuDeck saves with your cloud storage "
-                                                                  "provider.")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--setup", type=str, choices=["gdrive", "dropbox", "onedrive", "box"],
-                       help="Setup and login SaveSync for the specified provider")
-    group.add_argument("--sync", type=str, help="Run sync service")
-    args = vars(parser.parse_args())
-    if args["setup"] is not None:
-        p = pexpect.spawn(rclone, ["config"])
-        e = p.expect(["Current remotes:", "No remotes found, make a new one?"])
-        if e == 0 and pexpect.spawn(rclone, ["listremotes"]).expect(["saves:", pexpect.EOF]) == 0:
-            parser.error("You are already setup. remove from rclone config and try again")
-        else:
-            p.sendline("n")
-            p.expect("name>")
-            p.sendline("saves")
-            match args["setup"]:
-                case "gdrive":
-                    setup_gdrive(p)
-                case "dropbox":
-                    setup_dropbox(p)
-                case "onedrive":
-                    setup_onedrive(p)
-                case "box":
-                    setup_box(p)
-            p.expect("Yes this is OK")
-            p.sendline("y")
-            p.expect("Edit existing remote")
-            p.sendline("q")
-    elif args["sync"] is not None:
-        sync(args["sync"])
 
 
 def run_cmd(command):
@@ -65,15 +47,66 @@ def run_cmd(command):
         process.wait()
 
 
+rclone: str = resource_dir().joinpath("bin/rclone").__str__() \
+    if os.environ.get("APPDIR") is None else pathlib.Path(os.environ.get("APPDIR")).joinpath("usr/bin/rclone").__str__()
+unison: str = resource_dir().joinpath("bin/unison").__str__() \
+    if os.environ.get("APPDIR") is None else pathlib.Path(os.environ.get("APPDIR")).joinpath("usr/bin/unison").__str__()
+
+
+def main():
+    global conf
+    parser = argparse.ArgumentParser(prog="savesync", description="Syncs EmuDeck saves with your cloud storage "
+                                                                  "provider.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--setup", type=str, choices=["gdrive", "dropbox", "onedrive", "box"],
+                       help="Setup and login SaveSync for the specified provider")
+    group.add_argument("--sync", type=str, help="Run sync service")
+    args = vars(parser.parse_args())
+    run_cmd(["mkdir", "-p", F"{pathlib.Path.home()}/Emulation/tools/savesync"])
+    if not os.path.exists(F"{pathlib.Path.home()}/Emulation/tools/savesync/config.yml"):
+        with open(F"{pathlib.Path.home()}/Emulation/tools/savesync/config.yml", "w+") as f:
+            yaml.dump(default_schema.validate({}), f)
+    with open(F"{pathlib.Path.home()}/Emulation/tools/savesync/config.yml", "r") as f:
+        conf = yaml.load(f, Loader=yaml.Loader)
+        try:
+            schema.validate(conf)
+        except SchemaError as e:
+            print(e)
+        if args["setup"] is not None:
+            p = pexpect.spawn(rclone, ["config"])
+            e = p.expect(["Current remotes:", "No remotes found, make a new one?"])
+            if e == 0 and pexpect.spawn(rclone, ["listremotes"]).expect(["saves:", pexpect.EOF]) == 0:
+                parser.error("You are already setup. remove from rclone config and try again")
+            else:
+                p.sendline("n")
+                p.expect("name>")
+                p.sendline(conf.get("remote", "saves"))
+                match args["setup"]:
+                    case "gdrive":
+                        setup_gdrive(p)
+                    case "dropbox":
+                        setup_dropbox(p)
+                    case "onedrive":
+                        setup_onedrive(p)
+                    case "box":
+                        setup_box(p)
+                p.expect("Yes this is OK")
+                p.sendline("y")
+                p.expect("Edit existing remote")
+                p.sendline("q")
+        elif args["sync"] is not None:
+            sync(args["sync"])
+
+
 def sync(path_in: str):
     mount = F"{pathlib.Path.home()}/Emulation/tools/savesync/mount"
     run_cmd(["mkdir", "-p", mount])
-    run_cmd([rclone, "mkdir", "saves:/Emulation/saves/"])
-    run_cmd([rclone, "mount", "saves:/Emulation/saves/", mount, "--daemon"] +
-            shlex.split(os.environ.get("RCLONE_ARGS", "")))
-    run_cmd([unison, mount, path_in, "-repeat", "60", "-batch", "-copyonconflict",
-            "-prefer", "newer", "-links", "true", "-follow", "Name *"] +
-            shlex.split(os.environ.get("UNISON_ARGS", "")))
+    run_cmd([rclone, "mkdir", F"{conf.get('remote', 'saves')}:/Emulation/saves/"])
+    run_cmd([rclone, "mount", F"{conf.get('remote', 'saves')}:/Emulation/saves/", mount, "--daemon"] +
+            shlex.split(os.environ.get("RCLONE_ARGS", subprocess.list2cmdline(conf.get("rclone-args", "")))))
+    run_cmd([unison, mount, path_in, "-repeat", str(conf.get("sync-cooldown", 60)), "-batch", "-copyonconflict",
+             "-prefer", "newer", "-links", "true", "-follow", "Name *"] +
+            shlex.split(os.environ.get("UNISON_ARGS", subprocess.list2cmdline(conf.get("unison-args", "")))))
     run_cmd(["fusermount", "-u", mount])
 
 
